0b1ae3df20e4cbcc58ba4613375b74fad42a7913
wangdengfeng <1953819535@qq.com>
2025-09-05 11:10:01 +0800

    图标自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
+// 自动导入组件插件
 import Components from 'unplugin-vue-components/vite'
+// Iconify 解析器
+import IconsResolver from 'unplugin-icons/resolver'
+// Iconify 插件
+import Icons from 'unplugin-icons/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import PackageJson from './package.json' with { type: 'json' }
+
 
 process.env.VITE_APP_VERSION = PackageJson.version
 if (process.env.NODE_ENV === 'production') {
@@ -30,7 +36,13 @@ export default defineConfig({
     }),
     Components({
       dts: 'components.d.ts',
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({ prefix: 'i' }),
+      ],
+    }),
+    Icons({
+      autoInstall: true, // 自动安装缺失的图标包
     }),
     tailwindcss(),
   ],
